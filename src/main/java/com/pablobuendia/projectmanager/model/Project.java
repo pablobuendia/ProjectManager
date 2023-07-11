d1a6a9e90db3716cb4bed1802154675ea67719d0
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2023-07-11 01:13:03 -0300

    feat: create basic User and Project entities and repositories

---
+++ src/main/java/com/pablobuendia/projectmanager/model/Project.java
@@ -0,0 +1,43 @@
+package com.pablobuendia.projectmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "projects")
+public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "description")
+  private String description;
+
+  @ManyToMany
+  @JoinTable(name = "projects_users",
+      joinColumns = @JoinColumn(name = "project_id"),
+      inverseJoinColumns = @JoinColumn(name = "users_id"))
+  private Set<User> users = new HashSet<>();
+
+}
---
+++ src/main/java/com/pablobuendia/projectmanager/model/User.java
@@ -0,0 +1,35 @@
+package com.pablobuendia.projectmanager.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Email
+  @Column(name = "email", nullable = false, unique = true, length = 200)
+  private String email;
+
+}
