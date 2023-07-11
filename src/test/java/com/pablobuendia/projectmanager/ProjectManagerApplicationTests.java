56f6535548c4b716ef2b604c11e8cefd51ba8b13
Pablo Buendia <13057992+pablobuendia@users.noreply.github.com>
2023-07-10 22:54:22 -0300

    feat: Initial Spring Boot template generated with Spring Initializr

---
+++ src/test/java/com/pablobuendia/projectmanager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablobuendia.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
